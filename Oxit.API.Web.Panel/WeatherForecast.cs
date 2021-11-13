b7186ce165bf125f08e5932ea708af0a81a69004
Yaşar ARAZ <kodkopat@gmail.com>
2021-11-13 22:51:03 +0300

    angular eklendi

--- Oxit.API.Web.Panel/WeatherForecast.cs
+++ Oxit.API.Web.Panel/WeatherForecast.cs
@@ -9,5 +9,6 @@ namespace Oxit.API.Web.Panel
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+        public string? Summary2 { get; set; }
     }
 }
