08a8d265d8022d860d3107f1ed05a74f4a22e04c
Yaşar ARAZ <kodkopat@gmail.com>
2021-11-22 13:52:05 +0300

    MVC arayuzler eklendi

---
+++ Oxit.Web.Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Oxit.Web.Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
