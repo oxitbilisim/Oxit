4ef257c4135957f86f52b1d65d75334262cb53ef
Yaşar ARAZ <kodkopat@gmail.com>
2021-11-23 16:37:37 +0300

    admin panel updated

--- Oxit.UI.Web.Panel/src/main.ts
+++ Oxit.UI.Web.Panel/src/main.ts
@@ -2,10 +2,12 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { appSettings } from './appsettings';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule, {
