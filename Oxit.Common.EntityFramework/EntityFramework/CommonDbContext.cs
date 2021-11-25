15e62e2a884db7bae313fb2cfda2ceeec9456ba3
Yaşar ARAZ <kodkopat@gmail.com>
2021-11-25 11:43:11 +0300

    mig

--- Oxit.Common.EntityFramework/EntityFramework/CommonDbContext.cs
+++ Oxit.Common.EntityFramework/EntityFramework/CommonDbContext.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Oxit.Common.DataAccess.EntityFramework
 {
-    public class CommonDbContext : DbContext
+    public class CommonDbContext : BaseAppDbContext
     {
         public CommonDbContext()
              : base()
---
+++ Oxit.DataAccess/Migrations/20211125084030_personupdated.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Oxit.DataAccess.Migrations
+{
+    public partial class personupdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeleteDate",
+                table: "Person");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Person");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Person",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Person",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NationalId",
+                table: "Person",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Photo",
+                table: "Person",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Person");
+
+            migrationBuilder.DropColumn(
+                name: "NationalId",
+                table: "Person");
+
+            migrationBuilder.DropColumn(
+                name: "Photo",
+                table: "Person");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Person",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeleteDate",
+                table: "Person",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Person",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
