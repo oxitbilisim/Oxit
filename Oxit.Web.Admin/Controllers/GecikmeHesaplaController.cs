5bf628e578835809ce7b2bd64b7c76e3720d0e44
Hakan YILDIRIM <hakannyildirim@gmail.com>
2021-12-28 21:20:53 +0300

    update

--- Oxit.Web.Admin/Controllers/GecikmeHesaplaController.cs
+++ Oxit.Web.Admin/Controllers/GecikmeHesaplaController.cs
@@ -7,8 +7,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Oxit.DataAccess.EntityFramework;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace Oxit.Web.Admin.Controllers
 {
     [Route("api/[controller]")]
