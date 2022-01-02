8a5ad54e4ad8f6b300eced6be9df33dd47d39ae7
Şeref SEVEN <serefseven@gmail.com>
2022-01-02 16:25:41 +0300

    dashboard chart

---
+++ Oxit.Domain.Models/ChartLine.cs
@@ -0,0 +1,9 @@
+namespace Oxit.Domain.Models;
+
+public class ChartLine
+{
+    public string Date { get; set; }
+    public Double? Borc { get; set; }
+    public Double? Alacak { get; set; }
+    public Double? Gecikme { get; set; }
+}
--- Oxit.Web.Admin/Controllers/HomeController.cs
+++ Oxit.Web.Admin/Controllers/HomeController.cs
@@ -29,12 +29,34 @@ namespace Oxit.Web.Admin.Controllers
             var aktifFirmaSayi = appDbContext.HesapPlani.Where(h => h.Aktif).Count();
             var toplamBorc = appDbContext.Fis.Where(f => f.HesapPlani.Aktif && f.FisTip == FisTip.KiraFaturasi).Sum(b => b.Borc + b.GecikmeTutari);
             var toplamOdeme = appDbContext.Fis.Where(f => f.HesapPlani.Aktif && f.FisTip == FisTip.KiraOdemesi).Sum(b => b.Alacak);
-
+            List<ChartLine> chartData = appDbContext.Fis.AsEnumerable().GroupBy(f => String.Format("{0:MM yyyy}",f.Tarih)).Select(cl => new ChartLine
+            {
+                Date = String.Format("{0:MM-yyyy}",cl.First().Tarih),
+                Alacak = cl.Sum(c => c.Alacak),
+                Borc = cl.Sum(c => c.Borc),
+                Gecikme = cl.Sum(c => c.GecikmeTutari)
+            }).OrderBy(f => f.Date).Take(12).ToList();
+            List<string> labels = new List<string>();
+            List<Int32> borc = new List<Int32>();
+            List<Int32> alacak = new List<Int32>();
+            List<Int32> gecikme = new List<Int32>();
+            foreach (var item in chartData)
+            {
+                labels.Add(item.Date);
+                borc.Add(Convert.ToInt32((item.Borc.Value)));
+                alacak.Add(Convert.ToInt32((item.Alacak.Value)));
+                gecikme.Add(Convert.ToInt32((item.Gecikme.Value)));
+            }
+            
             Dictionary<string, object> model = new Dictionary<string, object>();
             model["aktifFirmaSayi"] = aktifFirmaSayi;
             model["toplamBorc"] = toplamBorc;
             model["toplamOdeme"] = toplamOdeme;
             model["kalanAlacak"] = toplamBorc-toplamOdeme;
+            model["labels"] = labels;
+            model["borc"] = borc;
+            model["alacak"] = alacak;
+            model["gecikme"] = gecikme;
             return View(model);
         }
     }
